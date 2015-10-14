444a83e72896269ae485eac664903887944c74b8
fazim <azimf94@gmail.com>
2015-10-14 15:49:41 -0400

    Update TrackModelInterface.java

--- src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
+++ src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
@@ -9,6 +9,10 @@ public interface TrackModelInterface {
 	public void getAuthority(int trainId, int authority);
 	public void getBlockOccupied(int blockId);
 	public void getBlockUnoccupied(int blockId);
+	public void getBeacon(int blockId);
+	
+	//will likely change how things are set but I assume other modules will mostly use the gets? please correct me 
+	//if I am wrong on Slack
 	public void setBeacon(int blockId);
 	public void setLayout();
 	public void setBlock(String line, String section, int blockId, int blockLen, int speedLim, String infra);
