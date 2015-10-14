ec1f036a597ce2a03c181393718496fc6d815ff7
fazim <azimf94@gmail.com>
2015-10-14 19:32:46 -0400

    Update TrackModelInterface.java

--- src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
+++ src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
@@ -5,14 +5,20 @@ package com.peanutbuttercookies.trainsystem.interfaces;
 */
 
 public interface TrackModelInterface {
-	public void getSpeed(int trainId, int speed);
-	public void getAuthority(int trainId, int authority);
-	public void getBlockOccupied(int blockId);
-	public void getBlockUnoccupied(int blockId);
+	//for train access, but should not need it
+	public int getSpeed(int trainId);
+	public int getAuthority(int trainId);
+	public int getBeacon(int trainId);
 	
+	
+	//trackController uses these; assuming prototype does not care for switches and lights
 	public void setSpeed(int trainId, int speed);
 	public void setAuthority(int trainId, int authority);
+	
+	//only handled by track model
 	public void setBeacon(int blockId);
-	public void setLayout(int blockId);
-	public void setBlock(String line, String section, int blockId, int blockLength, int speedLim, int infra, int occupancy);
+	public void setLayout(Block newBlock);
+	public void setBlock(String line, String section, int blockId, int blockLength, int speedLim, String infra, int occupancy);
+	public void getBlockOccupied(int blockId);
+	public void getBlockUnoccupied(int blockId);
 }
