e4bbe8219f4b983abc6e6bc36f2aec63a5909cc2
fazim <azimf94@gmail.com>
2015-10-14 15:32:37 -0400

    Update TrackModelInterface.java

--- src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
+++ src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
@@ -3,12 +3,14 @@
 *	10/14/2015
 */
 
+
 public interface TrackModelInterface {
 	public void getSpeed(int trainId, int speed);
 	public void getAuthority(int trainId, int authority);
 	public void getBlockOccupied(int blockId);
 	public void getBlockUnoccupied(int blockId);
-	public void setBeacon(int trainId, int blockId);
-	public void setLayout(block blockId);
-	public void setBlock(String line, String section, int BlockNum, int BlockLength, int SpeedLim, int Infra);
+	public void setBeacon(int blockId);
+	public void setLayout(Block blockId);
+	public void setBlock(String line, String section, int blockId, int blockLen, int speedLim, int infra);
 }
+
