dca65ba254dd67f0231750d1cd0f881c0cb19e24
kevinjnash <kevin.joseph.nash@gmail.com>
2015-10-14 18:51:54 -0400

    fixed a syntactical error

--- src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
+++ src/main/java/com/peanutbuttercookies/trainsystem/interfaces/TrackModelInterface.java
@@ -1,14 +1,15 @@
+package com.peanutbuttercookies.trainsystem.interfaces;
 /*
 *	Fauzul Azim
 *	10/14/2015
 */
 
-public interface trackModelInterface {
+public interface TrackModelInterface {
 	public void getSpeed(int trainId, int speed);
 	public void getAuthority(int trainId, int authority);
 	public void getBlockOccupied(int blockId);
 	public void getBlockUnoccupied(int blockId);
 	public void setBeacon(int blockId);
-	public void setLayout(block blockId);
+	public void setLayout(int blockId);
 	public void setBlock(String line, String section, int BlockNum, int BlockLength, int SpeedLim, int Infra);
 }
