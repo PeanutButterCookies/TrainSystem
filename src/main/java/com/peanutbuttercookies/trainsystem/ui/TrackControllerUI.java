62ef8bbaaf702489b4a52678e43ac9f61f8b3db0
Kevin <Kevin@150.212.119.196>
2015-10-15 17:23:29 -0400

    changed so exit doesnt close all

--- src/main/java/com/peanutbuttercookies/trainsystem/ui/TrackControllerUI.java
+++ src/main/java/com/peanutbuttercookies/trainsystem/ui/TrackControllerUI.java
@@ -45,7 +45,7 @@ public class TrackControllerUI extends JFrame {
 		super("Track Controller");
 		this.trackController = trackController;
 		setTitle("Track Controller Module");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 1280, 720);
 		contentPane = new JPanel();
 		contentPane.setBorder(null);
