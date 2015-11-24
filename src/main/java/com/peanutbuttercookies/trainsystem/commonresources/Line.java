a9ad690d3d633a9e6a9fe5e7703ea4f3856649da
cluelesspuppet <cdgjazz@comcast.net>
2015-11-24 13:43:42 -0500

    Updated line to include track controller objects

--- src/main/java/com/peanutbuttercookies/trainsystem/commonresources/Line.java
+++ src/main/java/com/peanutbuttercookies/trainsystem/commonresources/Line.java
@@ -2,9 +2,12 @@ package com.peanutbuttercookies.trainsystem.commonresources;
 
 import java.util.LinkedList;
 
+import com.peanutbuttercookies.trainsystem.trackcontroller.TrackController;
+
 public class Line {
 	private final LinkedList<Block> blocks;
 	private final String line;
+	private LinkedList<TrackController> controllers;
 	
 	public Line(String initLine, LinkedList<Block> initBlocks){
 		line	=initLine;
@@ -21,5 +24,18 @@ public class Line {
 	
 	public Block getBlock(int index){
 		return blocks.get(index);
+	}
+	
+	public void setTrackControllers(TrackController tc_1, TrackController tc_2){
+		controllers.add(tc_1);
+		controllers.add(tc_2);
+	}
+	
+	public LinkedList getAllTrackControllers(){
+		return controllers;
+	}
+	
+	public TrackController getTrackController(int index){
+		return controllers.get(index);
 	}
 }
