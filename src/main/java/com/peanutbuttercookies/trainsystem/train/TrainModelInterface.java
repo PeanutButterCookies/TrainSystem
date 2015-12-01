9fb120a67929973ba49417d88a70dcc6a7005328
Autumn Good <goodautumn95@gmail.com>
2015-12-01 13:30:40 -0500

    Additions
    Adding in new file

---
+++ src/main/java/com/peanutbuttercookies/trainsystem/train/TrainModelInterface.java
@@ -0,0 +1,23 @@
+package com.peanutbuttercookies.trainsystem.train;
+
+public interface TrainModelInterface {
+	void setBlock(Block block);
+	void setAngle(double angle);
+	void brakes();
+	void setSpeedLimits(double limit);
+	void setLights(String lights);
+	void setDoors(boolean doors);
+	void setPower(double power);
+	void emergencyBrakes();
+	void setSpeedAndAuth(double speed, int auth);
+	double getLength();
+	double getWidth();
+	double getHeight();
+	double getMass();
+	double getPower();
+	double getNumPassengers();
+	double getNumCars();
+	double getCurrentSpeed();
+	double getAcceleration();
+
+}
