0a4b0cb6ba483f1c54d4225878e3e3ea13a9f3e8
Kevin <Kevin@150.212.118.225>
2015-12-03 18:17:36 -0500

    things changed a bit

--- src/main/java/com/peanutbuttercookies/trainsystem/systemwrapper/MainApp.java
+++ src/main/java/com/peanutbuttercookies/trainsystem/systemwrapper/MainApp.java
@@ -26,7 +26,7 @@ public class MainApp {
 		TrackControllerStaticModule trackController = new TrackControllerStaticModule();
 		TrackModelInterface trackModel = new TrackModel();
 		//TODO
-		trackModel.fileRead("C:/Users/Chris/Documents/University of Pittsburgh/Junior Year/Software Engineering/ModifiedTrackLayout.xlsx");
+		trackModel.fileRead("C:/Users/Kevin/Downloads/ModifiedTrackLayout.xlsx");
 		
 		trackController.setCTC(ctc);
 		TrackControllerUI tcUI = new TrackControllerUI();
